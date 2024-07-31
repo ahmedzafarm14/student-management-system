1488d31e30f8245d7b63e16517060386fd1dd4c7
Ahmad Zafar <113054376+ahmedzafarm14@users.noreply.github.com>
2024-07-31 17:19:50 +0500

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyClngzjgujIwQc5DtI2F3Ivn78GNa11ZTs',
-    authDomain: 'student-management-system-427.firebaseapp.com',
-    projectId: 'student-management-system-427',
-    storageBucket: 'student-management-system-427.appspot.com',
-    messagingSenderId: '280267142669',
-    appId: '1:280267142669:web:18a4b8c397207b839a8744',
-    measurementId: 'G-7FPYT80306',
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: '',
 };
 
 const app = initializeApp(firebaseConfig);
